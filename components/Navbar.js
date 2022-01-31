2f04c1c5828812d0591683c417b1298214ed149d
shadid12 <shadidhaquee2012@gmail.com>
2022-01-31 01:00:30 -0500

    done 2.d

---
+++ components/Navbar.js
@@ -0,0 +1,37 @@
+// components/Navbar.js
+
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import Cookie from 'js-cookie'
+
+export default function Navbar() {
+  const router = useRouter()
+  const logOut = () => {
+      Cookie.remove('fauna-session')
+      router.push('/login')
+  }
+
+  return (
+    <nav className="uk-navbar-container" style={{ display: 'flex'}}>
+      <div className="uk-navbar-left">
+        <ul className="uk-navbar-nav">
+          <li className="uk-active"><Link href='/'>Fauna E-Com</Link></li>
+        </ul>
+      </div>
+      <div className="uk-navbar-right">
+        <ul className="uk-navbar-nav">
+          <li >
+            <Link href='/store/new' className="uk-button uk-button-primary" style={{ color: 'white'}}>
+              Add Store
+            </Link>
+          </li>
+          <li >
+            <a onClick={logOut} className="uk-button uk-button-danger" style={{ color: 'white'}}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-// pages/index.js
 import Dashboard from '../components/Dashboard'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
   return (
-    <div className={styles.container}>
-      <Dashboard />
-    </div>
+    <Layout>
+      <div className={styles.container}>
+        <Dashboard />
+      </div>
+    </Layout>
   )
 }
