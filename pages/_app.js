3a8db71b5fee2d3fb553d313335f6d513f6c3aa1
shadid12 <shadidhaquee2012@gmail.com>
2022-01-31 00:29:22 -0500

    done 2.b

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import Head from 'next/head';
 import { ApolloProvider } from '@apollo/client';
 import client from '../apollo-client';
 import '../styles/globals.css';
@@ -5,6 +7,11 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.6/dist/css/uikit.min.css" />
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.6/dist/js/uikit.min.js" />
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.6/dist/js/uikit-icons.min.js" />
+      </Head>
       <ApolloProvider client={client}>
         <Component {...pageProps} />
       </ApolloProvider>
